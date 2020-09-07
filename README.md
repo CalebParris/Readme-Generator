a04b9c28576d07511a97bf3f1f7f570f3675b3e8
Caleb Parris <calebparris@live.com>
2020-09-06 22:47:23 -0500

    Fully functioning readme generator

--- README.md
+++ README.md
@@ -1,28 +1,48 @@
-# Readme-Generator
+# Readme Generator
 
-## Description
-This repository will generate a readme file based off user input so that it can be used in any future projects you may have.
+   [](https://img.shields.io/badge/License-MIT-brightgreen)
 
-## Table of Contents
-* [Installation](#Installation)
-* [Usage](#Usage)
-* [License](#License)
-* [Contributing](#Contributing)
-* [Tests](#Tests)
-* [Questions](#Questions)
+   ## Description
+   This project with auto generate a readme file based off user input in the terminal
 
-## Installation
+   ## Table of Contents
+   * [Instalation](#Installation)
+   * [Usage](#Usage)
+   * [License](#License)
+   * [Contributing](#Contributing)
+   * [Tests](#Tests)
+   * [Questions](#Questions)
+   
+   ## Installation
+   To install the necessary dependencies, run the following code: npm install
 
-## Usage
+   ## Usage
+   To use this project and generate a readme, run the following code: node index.js
 
-## License
-[![GitHub](https://img.shields.io/github/license/CalebParris/Readme-Generator?style=plastic)](https://github.com/CalebParris/Readme-Generator/blob/master/LICENSE)
+   ## License
+   
+        MIT License
 
-## Contributing
-### Contributors
-[![Contributor Image](https://github.com/CalebParris.png?size=75)](https://github.com/CalebParris)
+        Copyright 2020 Caleb Parris
 
-## Tests
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-## Questions
-If you have any comments, questions, or concerns, please post them [here](https://github.com/CalebParris/Readme-Generator/issues) and I will respond as soon as I am able.
+   ## Contributing
+   To contribute to this project, please contact me following the instructions in the Questions section
+   ### Contributors
+   [![Contributor Image](https://github.com/CalebParris.png?size=75)](https://github.com/CalebParris)
+
+   ## Tests
+   To test this project, run the code from the Installation and Usage sections and fill out the terminal prompts with your information
+
+   ## Questions
+   If you have any comments, questions, or concerns about this project, please post them [here](https://github.com/CalebParris/Readme-Generator/issues) and I will respond as soon as I am able.
+
+   Otherwise, you can contact me through the following means:
+   * Email: calebparris@live.com
+   * Github: [CalebParris](https://github.com/CalebParris)
+    
--- index.js
+++ index.js
@@ -63,93 +63,106 @@ function promptUser(){
     ]);
 }
 
+//Generates the markdown code filled in with all the amswers from the terminal prompts
+function generateReadme(answers){
+   return `${`# ${answers.title}
+
+   [](https://img.shields.io/badge/License-${answers.license}-brightgreen)
+
+   ## Description
+   ${answers.description}
+
+   ## Table of Contents
+   * [Instalation](#Installation)
+   * [Usage](#Usage)
+   * [License](#License)
+   * [Contributing](#Contributing)
+   * [Tests](#Tests)
+   * [Questions](#Questions)
+   
+   ## Installation
+   ${answers.install}
+
+   ## Usage
+   ${answers.usage}
+
+   ## License
+   ${generateLicenseInfo(answers)}
+
+   ## Contributing
+   ${answers.contribute}
+   ### Contributors
+   [![Contributor Image](https://github.com/${answers.userName}.png?size=75)](https://github.com/${answers.userName})
+
+   ## Tests
+   ${answers.test}
+
+   ## Questions
+   If you have any comments, questions, or concerns about this project, please post them [here](https://github.com/${answers.userName}/${repoTitle(answers)}/issues) and I will respond as soon as I am able.
+
+   Otherwise, you can contact me through the following means:
+   * Email: ${answers.email}
+   * Github: [${answers.userName}](https://github.com/${answers.userName})`}
+    `
+}
+
+//Replaces the spaces in the title with dashes so that it can be added to the github url and link directly to the intended repository
+function repoTitle(answers){
+    answers.title = answers.title.replace(/\s+/g, "-");
+    return answers.title;
+}
+
+//Generates the information on the chosen license to be added in the License section
+function generateLicenseInfo(answers){
+    if (answers.license === "MIT"){
+        let licenseInfo = `
+        MIT License
+
+        Copyright ${currentYear()} ${answers.personName}
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+
+        return licenseInfo;
+
+    } else if (answers.license === "ISC"){
+        let licenseInfo = `
+        ISC License
+        
+        Copyright ${currentYear()} ${answers.personName}
+
+        Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+        
+        THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`;
+
+        return licenseInfo;
+    } else {
+        let licenseInfo = "There is no license for this project";
+
+        return licenseInfo;
+    }
+}
+
+//Returns the current year so that the copyright will be updated based off when the code is run
+function currentYear(){
+    let date = new Date();
+    let year = date.getFullYear();
+    return year;
+}
+
+//This runs the promts and the writes the necessary info to a markdown file based on the answers to the promted questions
 promptUser()
-//This is for testing purposes
 .then(function(answers){
-    console.log(answers);
+    const readMe = generateReadme(answers);
+
+    return writeFileAsync("Readme.md", readMe)
 })
-
-//Below is a work in progress of the generation of the readme
-
-// function generateReadme(answers){
-//     `
-//         # ${answers.title}
-
-//         [](https://img.shields.io/badge/License-${answers.license}-brightgreen)
-
-//         ## Description
-//         ${answers.description}
-
-//         ## Table of Contents
-//         * [Instalation](#Installation)
-//         * [Usage](#Usage)
-//         * [License](#License)
-//         * [Contributing](#Contributing)
-//         * [Tests](#Tests)
-//         * [Questions](#Questions)
-        
-//         ## Installation
-//         ${answers.install}
-
-//         ## Usage
-//         ${answers.usage}
-
-//         ## License
-//         ${license()}
-
-//         ## Contributing
-//         ${answers.contribute}
-//         ### Contributors
-//         [![Contributor Image](https://github.com/${answers.userName}.png?size=75)](https://github.com/${answers.userName})
-
-//         ## Tests
-//         ${answers.test}
-
-//         ## Questions
-//         If you have any comments, questions, or concerns about this project, please post them [here](https://github.com/${answers.userName}/${repoTitle()}/issues) and I will respond as soon as I am able.
-
-//         Otherwise, you can contact me through the following means:
-//         * Email: ${answers.email}
-//         * Github: [https://github.com/${answers.userName}](https://github.com/${answers.userName})
-//     `
-// }
-
-// function repoTitle(){
-//     answers.title = answers.title.replace(/\s+/g, "-");
-//     return answers.title;
-// }
-
-// function license(){
-//     if (answers.license === "MIT"){
-//         let licenseInfo = `Copyright ${currentYear()} ${answers.personName}
-
-//         Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-        
-//         The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-        
-//         THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
-
-//         return licenseInfo;
-
-//     } else if (answers.license === "ISC"){
-//         let licenseInfo = `Copyright ${currentYear()} ${answers.personName}
-
-//         Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
-        
-//         THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`;
-
-//         return licenseInfo;
-//     } else {
-//         let licenseInfo = "There is no license for this project";
-
-//         return licenseInfo;
-//     }
-// }
-
-// function currentYear(){
-//     let date = new Date();
-//     let year = date.getFullYear();
-//     return year;
-// }
-
-// promptUser();
+.then(function(){
+    console.log("Wrote to new file!");
+})
+.catch(function(err){
+    console.log(err);
+});
